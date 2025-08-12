759d359c71e81ac5046c953af30d4b183c47ee4b
SebaSilC <sebastian@playbookvisualarts.com>
2025-08-12 08:10:03 +0200

    Create main.tf

---
+++ terraform-test/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.7.5"
+}
+
+provider "null" {}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Hello Policy Check'"
+  }
+}
